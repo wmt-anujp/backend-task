238f7513a47a169f1b0eca2e2e892a69c130a289
Anuj Panchal <anujp@webmob.tech>
2022-03-14 11:45:26 +0530

    basics

--- basics.php
+++ basics.php
@@ -19,7 +19,9 @@
     </form>
 
     <?php
-
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     if (isset($_POST["add"])) {
         $num1 = $_POST["num1"];
         $num2 = $_POST["num2"];
