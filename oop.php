4ebf19b562df8047bfdf92992426fa3090dfa327
Anuj Panchal <anujp@webmob.tech>
2022-03-15 16:09:26 +0530

    oop

--- oop.php
+++ oop.php
@@ -46,7 +46,7 @@
     echo "<br>";
     echo "Color: " .  $apple->get_color();
     echo "<br>";
-    echo "<h3 style='color:lightgreen'>Costructor and Destructor</h3>";
+    echo "<h3 style='color:lightgreen'>Costructor, Destructor, Inheritance, Constants</h3>";
     class employee
     {
         public $name;
@@ -56,23 +56,25 @@
             $this->name = $name;
             $this->salary = $salary;
         }
-        public function intro()
+        final public function intro()
         {
             echo "Something about {$this->name} and their salary is {$this->salary}";
         }
         public function __destruct()
         {
             echo "Destruct function executed!<br>";
+            echo self::STRING;
         }
+        const STRING = "Just accessing constant<br>";
     }
     class webmob extends employee
     {
         public $address;
-        public function __construct()
+        public function __construct($name, $salary, $address)
         {
-            // $this->name = $name;
-            // $this->salary = $salary;
-            // $this->address = $address;
+            $this->name = $name;
+            $this->salary = $salary;
+            $this->address = $address;
         }
         public function message()
         {
@@ -80,9 +82,9 @@
             $this->intro();
         }
     }
-
-    $anuj = new webmob("Anuj", 80000);
-    $anuj->message();
+    echo employee::STRING;
+    $WebMob = new webmob("Anuj", 80000, "some");
+    $WebMob->message();
     // $panchal = new employee("Panchal", 90000);
     // echo "Salary of Anuj is $anuj->salary <br>";
     // echo "Salary of Panchal is $panchal->salary";
@@ -94,7 +96,99 @@
     $mango->colors = 'Yellow';
     $mango->weights = '300';
     echo $mango->names . "<br>";
-    echo "<h3 style='color:lightgreen'>Inheritance</h3>";
+    echo "<h3 style='color:lightgreen'>Abstract Class</h3>";
+    abstract class Car
+    {
+        public $name;
+        public function __construct($name)
+        {
+            $this->name = $name;
+        }
+        abstract public function intro(): string;
+    }
+
+    class Audi extends Car
+    {
+        public function intro(): string
+        {
+            return "Choose German quality! I'm an $this->name!";
+        }
+    }
+
+    class Volvo extends Car
+    {
+        public function intro(): string
+        {
+            return "Proud to be Swedish! I'm a $this->name!";
+        }
+    }
+
+    class Citroen extends Car
+    {
+        public function intro(): string
+        {
+            return "French extravagance! I'm a $this->name!";
+        }
+    }
+
+    $audi = new audi("Audi");
+    echo $audi->intro();
+    echo "<br>";
+
+    $volvo = new volvo("Volvo");
+    echo $volvo->intro();
+    echo "<br>";
+
+    $citroen = new citroen("Citroen");
+    echo $citroen->intro();
+
+    echo "<h3 style='color:lightgreen'>Traits</h3>";
+    trait messages
+    {
+        public function msg1()
+        {
+            echo "message 1 is message me<br>";
+        }
+        public function msg2()
+        {
+            echo "message 2 is message me<br>";
+        }
+    }
+    class manual
+    {
+        use messages;
+    }
+    $obj = new manual();
+    $obj->msg1() . "<br>";
+    $obj->msg2() . "<br>";
+
+    echo "<h3 style='color:lightgreen'>Static Methods and Properties</h3>";
+    class greeting
+    {
+        public static $numeric = 58;
+        public static function warning()
+        {
+            echo "This is a warning using static methods<br>";
+            echo self::$numeric . '<br>';
+        }
+        // public function __construct()
+        // {
+        //     // self::warning();
+        // }
+    }
+    class temporary extends greeting
+    {
+        public static function alert()
+        {
+            greeting::warning();
+            return parent::$numeric;
+        }
+    }
+    echo temporary::$numeric . '<br>';
+    $tmp_obj = new greeting();
+    $tmp_obj2 = new temporary();
+    $tmp_obj2->alert();
+    // greeting::warning();
     ?>
 </body>
 
