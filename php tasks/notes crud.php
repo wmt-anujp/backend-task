443beb005576adc96666c54f091384e407c9cace
Anuj Panchal <anujp@webmob.tech>
2022-03-24 18:17:35 +0530

    final

--- php tasks/notes crud.php
+++ php tasks/notes crud.php
@@ -10,9 +10,34 @@
 </head>
 
 <body>
-    <div class="container">
+    <?php
+    $connection = mysqli_connect("localhost", "root", "", "notes_crud") or die("Connection Failed");
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $query = "INSERT INTO `Notes data`(`Title`, `Description`) VALUES ('$title','$description')";
+        $query_execute = mysqli_query($connection, $query);
+        // $count = mysqli_num_rows($query_execute);
+        if ($query_execute) {
+            echo "<script>alert('Your Notes were added')</script>";
+            sleep(3);
+            // header("Location:show_data.php");
+            // echo '<div class="container alert alert-success alert-dismissible" role="alert">
+            // <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            // <strong>Success!</strong> Your Note was added!
+            // </div>';
+        } else {
+            echo "<script>alert('Your Notes were not added')</script>";
+            // echo '<div class="container alert alert-danger alert-dismissible fade show" role="alert">
+            // <strong>ERROR!</strong> Your note was not added!
+            // <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            // </div>';
+        }
+    }
+    ?>
+    <div class="container mt-3">
         <h4>Add Your Notes</h4>
-        <form action="show_data.php" method="POST">
+        <form action="" method="POST">
             <div class="form-group mt-3">
                 <label for="title" class="form-label">Notes Title</label>
                 <input type="text" style="width: 350px;" class="form-control" id="title" name="title" maxlength="20" onkeypress="return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123) || (event.charCode==32)" required>
@@ -22,33 +47,10 @@
                 <textarea class="form-control" style="width: 350px;" name="description" id="description" cols="30" rows="5" maxlength="254" required></textarea>
             </div>
             <div class="mt-3">
-                <button type="submit" class="btn btn-success">Add Note</button>
+                <button type="submit" class="btn btn-success" name="addnote">Add Note</button>
             </div>
         </form>
     </div>
-    <?php
-    $connection = mysqli_connect("localhost", "root", "", "notes_crud") or die("Connection Failed");
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $title = $_POST['title'];
-        $description = $_POST['description'];
-
-        $query = "INSERT INTO `Notes data`(`Title`, `Description`) VALUES ('$title','$description')";
-        $query_execute = mysqli_query($connection, $query);
-        $count = mysqli_num_rows($query_execute);
-        // echo $count;
-        if ($query_execute) {
-            echo '<div class="container alert alert-success alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <strong>Success!</strong> Your Note was added!
-            </div>';
-        } else {
-            echo '<div class="container alert alert-danger alert-dismissible fade show" role="alert">
-            <strong>ERROR!</strong> Your note was not added!
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>';
-        }
-    }
-    ?>
 </body>
 
 </html>
